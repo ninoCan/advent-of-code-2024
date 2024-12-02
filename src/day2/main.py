77c16f9675f5f73047421b87786dc00546a1f718
Anto"nino" Cangialosi <ninocangialosi@yahoo.it>
2024-12-02 11:52:33 +0100

    Update main.py

--- src/day2/main.py
+++ src/day2/main.py
@@ -1,22 +1,22 @@
 import re
 from itertools import pairwise
 from pathlib import Path
+from typing import Optional
 
 from src.utils import parse_all
 
 type Level = list[int]
 
-def extract_pattern(line: str, pattern: re.Pattern[str]) -> Level:
-   assert pattern # ignore usage for now
+def extract_pattern(line: str, pattern: Optional[re.Pattern[str]] = None) -> Level:
    return [int(item) for item in line.split(" ")]
 
 
 def is_safe(level: Level) -> bool:
     deltas = [int(right) - int(left) for left, right in pairwise(level)]
     if level[0] < level[1]:
-        is_valid = lambda x: x >= 1 and x <= 3
+        is_level_safe = lambda x: x >= 1 and x <= 3
         return all([is_valid(item) for item in deltas])
-    is_valid = lambda x: x >= -3 and x <= -1
+    is_level_safe = lambda x: x >= -3 and x <= -1
     return all([is_valid(item) for item in deltas])
 
 def is_safe_with_damper(level: Level) -> bool:
@@ -29,7 +29,6 @@ def is_safe_with_damper(level: Level) -> bool:
     return False
 
 def main(input_lines: list[str]) -> int:
-    pattern = re.compile(r"\d+")
     all_levels = parse_all(input_lines, extract_pattern, pattern)
     safety_results = [is_safe(level) for level in all_levels]
     return safety_results.count(True)
