0876effbf84c2386b7354fd9479ac60d255580d1
Antonino Cangialosi <ninocangialosi@yahoo.it>
2024-12-08 18:06:03 +0100

    feat: add correct solution for part 2

--- src/day8/main.py
+++ src/day8/main.py
@@ -15,10 +15,25 @@ class Solution:
             self.lines = file.readlines() if not lines else lines
 
     @staticmethod
-    def get_antinodes(grid: Grid, left: Point, right: Point) -> list[Point]:
+    def get_first_two_antinodes(grid: Grid, left: Point, right: Point) -> list[Point]:
         one = Point(*[2 * coord_l - coord_r for coord_l, coord_r in zip(left, right)])
         two = Point(*[2 * coord_r - coord_l for coord_l, coord_r in zip(left, right)])
         return [point for point in (one, two) if grid.is_inside(point)]
+
+    @staticmethod
+    def get_all_antinodes(grid: Grid, left: Point, right: Point) -> list[Point]:
+        delta = Point(*[coord_l - coord_r for coord_l, coord_r in zip(left, right)])
+        above = [Point(*right)]
+        candidate_above = Point(*[coord_r - coord_d for coord_r, coord_d in zip(right, delta)])
+        while grid.is_inside(candidate_above):
+            above.append(candidate_above)
+            candidate_above = Point(*[coord_r - coord_d for coord_r, coord_d in zip(candidate_above, delta)])
+        below = [Point(*left)]
+        candidate_below = Point(*[coord_l + coord_d for coord_l, coord_d in zip(left, delta)])
+        while grid.is_inside(candidate_below):
+            below.append(candidate_below)
+            candidate_below = Point(*[coord_l + coord_d for coord_l, coord_d in zip(candidate_below, delta)])
+        return above + below
 
     def first_task(self) -> int:
         grid = Grid(self.lines)
@@ -28,14 +43,25 @@ class Solution:
             if item not in (".", "\n")
         }
         antinodes = set(flatten([
-            self.get_antinodes(grid, *pair)
+            self.get_first_two_antinodes(grid, *pair)
             for frequency in antennas.keys()
             for pair in combinations(antennas[frequency], 2)
         ]))
         return len(antinodes)
 
     def second_task(self) -> int:
-        pass
+        grid = Grid(self.lines)
+        antennas = {
+            item: grid.locate(item)
+            for item in Counter("".join(flatten(grid.data.tolist()))).keys()
+            if item not in (".", "\n")
+        }
+        antinodes = set(flatten([
+            self.get_all_antinodes(grid, *pair)
+            for frequency in antennas.keys()
+            for pair in combinations(antennas[frequency], 2)
+        ]))
+        return len(antinodes)
 
 def main():
     solution = Solution()
