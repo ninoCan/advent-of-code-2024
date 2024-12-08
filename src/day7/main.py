ad061263965e6c87f307e31f6241c8bba03dc613
Antonino Cangialosi <ninocangialosi@yahoo.it>
2024-12-08 15:14:43 +0100

    feat: add correct solution for part 1

--- src/day7/main.py
+++ src/day7/main.py
@@ -1,5 +1,9 @@
+import re
 from pathlib import Path
 from typing import Optional
+
+from src.utils import parse_all
+
 
 class Solution:
     _STANDARD_PATH = Path(__file__).parent / "input.txt"
@@ -8,8 +12,23 @@ class Solution:
         with open(path) as file:
             self.lines = file.readlines() if not lines else lines
 
+    def generate_combinations(self, current: str, remaining_numbers: list[str]) -> list[str]:
+        if not remaining_numbers:
+            return [current]
+        next_number, *rest = remaining_numbers
+        sum_expr = f"({current} + {next_number})"
+        prod_expr = f"({current} * {next_number})"
+        return self.generate_combinations(sum_expr, rest) + self.generate_combinations(prod_expr, rest)
+
+    def extract_valid_test_values(self, line: str, pattern: re.Pattern[str]) -> str:
+        test_value, *numbers = [el for el in pattern.findall(line)]
+        combos_results = [eval(combo) for combo in self.generate_combinations(numbers[0], numbers[1:])]
+        return test_value if combos_results.count(int(test_value)) else "0"
+
     def first_task(self) -> int:
-        pass
+        pattern = re.compile(r"\d+")
+        valid_test_values = parse_all(self.lines, self.extract_valid_test_values, pattern)
+        return sum([int("".join(test_value)) for test_value in valid_test_values])
 
 
     def second_task(self) -> int:
