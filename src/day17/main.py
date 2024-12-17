e0db33b2c5fa4b560f1621c785a22ebf164364d3
Antonino Cangialosi <ninocangialosi@yahoo.it>
2024-12-18 00:18:55 +0100

    feat: add correct solution for part 1

--- src/day17/main.py
+++ src/day17/main.py
@@ -1,24 +1,140 @@
+import re
+from enum import Enum
 from pathlib import Path
 from typing import Optional
+
+from icecream import ic
+
+
+class OpCodes(Enum):
+    adv = 0
+    bxl = 1
+    bst = 2
+    jnz = 3
+    bxc = 4
+    out = 5
+    bdv = 6
+    cdv = 7
+
+
+class Program:
+    def __init__(
+        self, data: list[str] = None, A=None, B=None, C=None, instructions=None
+    ):
+        _pattern = re.compile(r"\d+")
+        self.register_A = int(_pattern.findall(data[0])[0]) if data else A
+        self.register_B = int(_pattern.findall(data[1])[0]) if data else B
+        self.register_C = int(_pattern.findall(data[2])[0]) if data else C
+        self._instructions = (
+            re.compile(r"Program: ([\d,]+)").findall(data[4])[0]
+            if data
+            else instructions
+        )
+        self.instructions = [int(char) for char in self._instructions if char != ","]
+        self.pointer = 0
+        self.output = []
+
+    def run(self) -> str:
+        while self.pointer < len(self.instructions):
+            opcode = self.instructions[self.pointer]
+            operand = self.instructions[self.pointer + 1]
+            op = getattr(self, OpCodes(opcode).name)
+            output = op(operand)
+            if output:
+                self.output.append(output)
+            if opcode != OpCodes.jnz.value:
+                self.pointer += 2
+            self.run()
+        return ",".join(self.output)
+
+    def combo(self, operand):
+        match operand:
+            case s if s in {0, 1, 2, 3}:
+                return s
+            case 4:
+                return self.register_A
+            case 5:
+                return self.register_B
+            case 6:
+                return self.register_C
+            case 7:
+                pass
+
+    def _division(self, operand) -> int:
+        return self.register_A // (2 ** self.combo(operand))
+
+    def adv(self, operand):
+        division = self._division(operand)
+        self.register_A = division
+        return None
+
+    def bxl(self, operand):
+        xor = self.register_B.__xor__(operand)
+        self.register_B = xor
+        return None
+
+    def bst(self, operand):
+        module = self.combo(operand) % 8
+        self.register_B = module
+        return None
+
+    def jnz(self, operand):
+        if self.register_A == 0:
+            self.pointer += 2
+            return
+        self.pointer = operand
+        return None
+
+    def bxc(self, operand):
+        _ = operand
+        xor = self.register_B.__xor__(self.register_C)
+        self.register_B = xor
+        return None
+
+    def out(self, operand):
+        modulo = self.combo(operand) % 8
+        return str(modulo)
+
+    def bdv(self, operand):
+        division = self._division(operand)
+        self.register_B = division
+        return None
+
+    def cdv(self, operand):
+        division = self._division(operand)
+        self.register_C = division
+        return None
+
+    def revert(self):
+        pass
+
 
 class Solution:
     _STANDARD_PATH = Path(__file__).parent / "input.txt"
 
-    def __init__(self, path: Path=_STANDARD_PATH, lines: Optional[list[str]] = None):
+    def __init__(self, path: Path = _STANDARD_PATH, lines: Optional[list[str]] = None):
         with open(path) as file:
             self.lines = file.readlines() if not lines else lines
 
-    def first_task(self) -> int:
-        pass
-
+    def first_task(self) -> str:
+        pc = Program(self.lines)
+        return pc.run()
 
     def second_task(self) -> int:
-        pass
+        pc = Program(self.lines)
+        answer = 0
+        while pc._instructions != ",".join(pc.output):
+            pc = Program(A=answer, B=0, C=0, instructions=pc.instructions)
+            ic(pc.run())
+            answer += 1
+        return answer
+
 
 def main():
     solution = Solution()
     print("The first answer is", solution.first_task())
     print("The second answer is", solution.second_task())
+
 
 if __name__ == "__main__":
     main()
--- tests/day17/test_main.py
+++ tests/day17/test_main.py
@@ -5,14 +5,22 @@ from pathlib import Path
 
 import pytest
 
-from src.day17.main import Solution
+from src.day17.main import Solution, Program
 
 
 @pytest.fixture
 def provide_test_lines() -> list[str]:
     source_path = Path(getsourcefile(Solution)).resolve().parent / "README.md"
     with source_path.open("r") as file:
-        example_slice = slice(104, 109)
+        example_slice = slice(109, 114)
+        return [line.strip() for line in file.readlines()[example_slice]]
+
+
+@pytest.fixture
+def provide_second_test_lines() -> list[str]:
+    source_path = Path(getsourcefile(Solution)).resolve().parent / "README.md"
+    with source_path.open("r") as file:
+        example_slice = slice(130, 135)
         return [line.strip() for line in file.readlines()[example_slice]]
 
 
@@ -23,8 +31,46 @@ def test_first_task(provide_test_lines: list[str]) -> None:
     assert actual == expected
 
 
-def test_second_task(provide_test_lines: list[str]) -> None:
-    under_test = Solution(lines=provide_test_lines)
-    expected = "placeholder"
+def test_example_1() -> None:
+    under_test = Program(C=9, instructions="2,6")
+    expected = 1
+    _ = under_test.run()
+    actual = under_test.register_B
+    assert actual == expected
+
+
+def test_example_2() -> None:
+    under_test = Program(A=10, instructions="5,0,5,1,5,4")
+    expected = "0,1,2"
+    actual = under_test.run()
+    assert actual == expected
+
+
+def test_example_3() -> None:
+    under_test = Program(A=2024, instructions="0,1,5,4,3,0")
+    expected = "4,2,5,6,7,7,7,7,3,1,0"
+    actual = under_test.run()
+    assert actual == expected
+
+
+def test_example_4() -> None:
+    under_test = Program(B=29, instructions="1,7")
+    expected = 26
+    _ = under_test.run()
+    actual = under_test.register_B
+    assert actual == expected
+
+
+def test_example_5() -> None:
+    under_test = Program(B=2024, C=43690, instructions="4,0")
+    expected = 44354
+    _ = under_test.run()
+    actual = under_test.register_B
+    assert actual == expected
+
+
+def test_second_task(provide_second_test_lines) -> None:
+    under_test = Solution(lines=provide_second_test_lines)
+    expected = 117440
     actual = under_test.second_task()
     assert actual == expected
