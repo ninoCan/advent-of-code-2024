ae1ec2a7fadd11e06bfab0a0aa7c5c8a1239dc00
Antonino Cangialosi <ninocangialosi@yahoo.it>
2024-12-18 08:34:41 +0100

    feat: add new class for solving part 1

--- src/day18/main.py
+++ src/day18/main.py
@@ -1,24 +1,63 @@
+import re
 from pathlib import Path
-from typing import Optional
+from typing import Optional, Self
+
+from src.utils import Grid, Point
+
+
+class MemoryMap(Grid):
+    _WALL = "#"
+    _EMPTY = "."
+
+    def __init__(self, data: list[str], width: int, height: int):
+        self.width, self.height = width, height
+        super().__init__([self._EMPTY * self.width for _ in range(height)])
+        self._data = data
+        self._faults = []
+
+    @property
+    def faults(self) -> list[Point]:
+        if self._faults:
+            return self._faults
+        pattern = re.compile(r"\d+")
+        parsed_faults = []
+        for line in self._data:
+            coords = pattern.findall(line)
+            parsed_faults.append(Point(int(coords[1]), int(coords[0])))
+        return parsed_faults
+
+    def copy(self) -> Self:
+        new = MemoryMap([], self.width, self.height)
+        new._faults = self.faults
+        return new
+
+    def generate_memory_faults(self, number=None) -> Self:
+        new_memory = self.copy()
+        faults_to_add = self.faults[:number] if number else self.faults
+        for point in faults_to_add:
+            new_memory.data[*point] = self._WALL
+        return new_memory
+
 
 class Solution:
     _STANDARD_PATH = Path(__file__).parent / "input.txt"
 
-    def __init__(self, path: Path=_STANDARD_PATH, lines: Optional[list[str]] = None):
+    def __init__(self, path: Path = _STANDARD_PATH, lines: Optional[list[str]] = None):
         with open(path) as file:
             self.lines = file.readlines() if not lines else lines
 
     def first_task(self) -> int:
         pass
 
-
     def second_task(self) -> int:
         pass
+
 
 def main():
     solution = Solution()
     print("The first answer is", solution.first_task())
     print("The second answer is", solution.second_task())
+
 
 if __name__ == "__main__":
     main()
--- tests/day18/test_main.py
+++ tests/day18/test_main.py
@@ -5,7 +5,7 @@ from pathlib import Path
 
 import pytest
 
-from src.day18.main import Solution
+from src.day18.main import Solution, MemoryMap
 
 
 @pytest.fixture
@@ -14,6 +14,21 @@ def provide_test_lines() -> list[str]:
     with source_path.open("r") as file:
         example_slice = slice(25, 50)
         return [line.strip() for line in file.readlines()[example_slice]]
+
+
+@pytest.fixture
+def provide_test_map() -> list[str]:
+    source_path = Path(getsourcefile(Solution)).resolve().parent / "README.md"
+    with source_path.open("r") as file:
+        example_slice = slice(72, 79)
+        return [line.strip() for line in file.readlines()[example_slice]]
+
+
+def test_hurdle_map_generation(provide_test_lines, provide_test_map):
+    under_test = MemoryMap(provide_test_lines, 7, 7)
+    expected = provide_test_map
+    actual = ["".join(row) for row in under_test.generate_memory_faults(12).data]
+    assert actual == expected
 
 
 def test_first_task(provide_test_lines: list[str]) -> None:
