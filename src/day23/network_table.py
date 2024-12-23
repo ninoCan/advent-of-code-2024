556ea739faaf079a3a5e234b27e40dd59447f07a
Antonino Cangialosi <antonino.cangialosi@agilelab.it>
2024-12-23 21:33:05 +0100

    feat: submit solution to second part of day 23

--- src/day23/network_table.py
+++ src/day23/network_table.py
@@ -1,4 +1,5 @@
 from collections import defaultdict
+from itertools import groupby
 
 
 class NetworkTable:
@@ -22,3 +23,29 @@ class NetworkTable:
                     parties.add(",".join(sorted([key, conn, third])))
         self._lan_parties = parties
         return parties
+
+    @property
+    def party_clusters(self) -> set[str]:
+        grouped = defaultdict(set)
+        for key, group in groupby(self.lan_parties, lambda x: x[0:2]):
+            grouped[key].update(list(group))
+        candidates = {
+            key: { player for triple in game for player in triple.split(",")}
+            for key, game in grouped.items()
+            if len(game) > 1
+        }
+        clusters = {
+            ",".join(sorted(elements)): elements for elements in candidates.values()
+            if self.is_cluster(elements)
+        }
+        return set(clusters.keys())
+
+    def is_cluster(self, candiate_set: set[str]) -> bool:
+        connected_set = set.union(*[
+            self.adjacency_table[candidate]
+            for candidate in candiate_set
+        ])
+        for candidate in candiate_set:
+            layer = { candidate }.union(self.adjacency_table[candidate])
+            connected_set = connected_set.intersection(layer)
+        return connected_set == candiate_set
