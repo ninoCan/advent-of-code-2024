0d17b2fca32c9fa10260f1bb18c615d1555ee300
Antonino Cangialosi <antonino.cangialosi@agilelab.it>
2024-12-24 07:54:25 +0100

    test: complete test cases for part 1

--- tests/day24/test_main.py
+++ tests/day24/test_main.py
@@ -8,19 +8,31 @@ import pytest
 from src.day24.main import Solution
 
 
-
 @pytest.fixture
 def provide_test_lines() -> list[str]:
     source_path = Path(getsourcefile(Solution)).resolve().parent / 'README.md'
     with source_path.open("r") as file:
-        example_slice = slice("n - 1", "m")
+        example_slice = slice(64, 111)
         return [line.strip() for line in file.readlines()[example_slice]]
 
 
+@pytest.fixture
+def provide_small_test_lines() -> list[str]:
+    source_path = Path(getsourcefile(Solution)).resolve().parent / 'README.md'
+    with source_path.open("r") as file:
+        example_slice = slice(29, 39)
+        return [line.strip() for line in file.readlines()[example_slice]]
+
+
+def test_small_task(provide_small_test_lines: list[str]) -> None:
+    under_test = Solution(lines=provide_small_test_lines)
+    expected = 4
+    actual = under_test.first_task()
+    assert actual == expected
 
 def test_first_task(provide_test_lines: list[str]) -> None:
     under_test = Solution(lines=provide_test_lines)
-    expected = "placeholder"
+    expected = 2024
     actual = under_test.first_task()
     assert actual == expected
 
